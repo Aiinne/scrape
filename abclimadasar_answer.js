a311dfeb28e2f68d95c2410c4c261b0107ac4d9f
Qisyana <79745902+qisyana@users.noreply.github.com>
2021-08-05 05:38:11 +0700

    Update and rename family100_answer.js to abclimadasar_answer.js

--- abclimadasar_answer.js
+++ abclimadasar_answer.js
@@ -9,9 +9,9 @@ module.exports = {
         let text = m.text.toLowerCase().replace(/[^\w\s\-]+/, '')
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (!isSurrender) {
-            let index = room.jawaban.findIndex(v => v.toLowerCase().replace(/[^\w\s\-]+/, '') === text)
+            let index = room.huruf.findIndex(v => v.toLowerCase().replace(/[^\w\s\-]+/, '') === text)
             if (index < 0) {
-                if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply('Dikit lagi!')
+                if (Math.max(...room.huruf.filter((_, index) => !room.terjawab[index]).map(huruf => similarity(huruf, text))) >= threshold) m.reply('Dikit lagi!')
                 return !0
             }
             if (room.terjawab[index]) return !0
@@ -21,13 +21,13 @@ module.exports = {
         }
         let isWin = room.terjawab.length === room.terjawab.filter(v => v).length
         let caption = `
-*Soal:* ${room.soal}
-Terdapat *${room.jawaban.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
+*Soal inisial :* ${room.soal}
+Terdapat *${room.huruf.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
 (beberapa jawaban terdapat spasi)
 `: ''}
-${isWin ? `*SEMUA JAWABAN TERJAWAB*` : isSurrender ? '*MENYERAH!*' : ''}
-${Array.from(room.jawaban, (jawaban, index) => {
-            return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
+${isWin ? `*SEMUA JAWABAN TERJAWAB*\n ${room.jawaban}` : isSurrender ? '*MENYERAH!*' : ''}
+${Array.from(room.huruf, (huruf, index) => {
+            return isSurrender || room.terjawab[index] ? `(${index + 1}) ${huruf} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''} ✅`.trim() : false
         }).filter(v => v).join('\n')}
 ${isSurrender ? '' : `+${room.winScore} XP tiap jawaban benar`}
     `.trim()
