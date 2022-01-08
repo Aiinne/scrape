f70efc73f47f55bff16544c22de7297734150933
Qisyana <79745902+qisyana@users.noreply.github.com>
2022-01-08 12:42:31 +0700

    Create tebakgame.js

---
+++ tebakgame.js
@@ -0,0 +1,108 @@
+[
+
+    {
+        "img": "https://wallpapercave.com/wp/wp7756609.jpg",
+        "jawaban": "Pou"
+        },
+    {
+        "img": "https://images3.alphacoders.com/863/863236.jpg",
+        "jawaban": "Clash of Clans"
+        },
+    {
+        "img": "https://images8.alphacoders.com/855/855972.jpg",
+        "jawaban": "Clash Royale"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp1972779.png",
+        "jawaban": "Boom Beach"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp3624905.png",
+        "jawaban": "Hay Day"
+        },
+    {
+        "img": "https://images4.alphacoders.com/100/1005690.jpg",
+        "jawaban": "Grand Theft Auto San Andreas"
+        },
+    {
+        "img": "https://images6.alphacoders.com/117/1173817.jpg",
+        "jawaban": "Genshin Impact"
+        },
+    {
+        "img": "https://images4.alphacoders.com/113/1131196.jpg",
+        "jawaban": "Honkai Impact"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp2315446.jpg",
+        "jawaban": "Subway Surfers"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp2444769.jpg",
+        "jawaban": "Temple Run"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp2403882.png",
+        "jawaban": "My Talking Tom"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp2418149.jpg",
+        "jawaban": "Talking Angela"
+        },
+    {
+        "img": "https://upload.wikimedia.org/wikipedia/commons/5/5e/Piano_Tiles_2_logo.png",
+        "jawaban": "Piano Tiles 2"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp7442422.jpg",
+        "jawaban": "Henry Stickmin"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp1899269.jpg",
+        "jawaban": "Lost Saga"
+        },
+    {
+        "img": "https://p4.wallpaperbetter.com/wallpaper/760/328/984/zjsstc-iron-saga-official-long-hair-wallpaper-preview.jpg",
+        "jawaban": "Iron Saga"
+        },
+    {
+        "img": "https://images7.alphacoders.com/423/423177.jpg",
+        "jawaban": "Minecraft"
+        },
+    {
+        "img": "https://images3.alphacoders.com/594/594233.jpg",
+        "jawaban": "Resident Evil 4"
+        },
+    {
+        "img": "https://wallpapercave.com/wp/wp1898996.jpg",
+        "jawaban": "Grand Theft Auto Vice City"
+        },
+    {
+        "img": "https://images4.alphacoders.com/823/823198.jpg",
+        "jawaban": "Grand Theft Auto V"
+        },
+    {
+        "img": "https://images4.alphacoders.com/242/242928.jpg",
+        "jawaban": "Guitar Hero"
+        },
+    {
+        "img": "https://images.alphacoders.com/655/655419.jpg",
+        "jawaban": "Bully"
+        },
+    {
+        "img": "https://images.alphacoders.com/512/512466.jpg",
+        "jawaban": "League of Legends"
+        },
+    {
+        "img": "https://images2.alphacoders.com/474/474206.jpg",
+        "jawaban": "DotA II"
+        },
+    {
+        "img": "https://c4.wallpaperflare.com/wallpaper/274/29/113/digital-art-drawing-people-nier-automata-wallpaper-preview.jpg",
+        "jawaban": "Mobile Legends Bang Bang"
+        },
+    {
+        "img": "https://images3.alphacoders.com/816/816720.jpg",
+        "jawaban": "PlayerUnknown's Battleground"
+        }
+
+]
